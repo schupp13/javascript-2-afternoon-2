73654005b6c7f300d8dc4a89384bedb7b89db4fc
Philip Schultz <24783715+schupp13@users.noreply.github.com>
2019-06-19 16:27:08 -0400

    All graded question are finished, will return to finish extra credit

--- practice.js
+++ practice.js
@@ -18,7 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
+function first(arr){
+return arr[0];
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -33,7 +35,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last(arr){
+  return arr[arr.length-1]
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -48,6 +52,12 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
+function looper(family){
+  for(let i = 0; i < family.length; i++){
+    alert(family[i]);
+  }
+}
+
 
 
 
@@ -63,6 +73,12 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
+function reversedLooper(letters){
+  for (let i = letters.length -1  ; i >= 0; i--){
+    alert(letters[i]);
+  }
+}
+
 
 
 
@@ -78,11 +94,13 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
-
-
-
-
-
+function evenFinder(nums){
+  evenNums = [];
+  for(let i = 0; i < nums.length ; i++){
+    nums[i] % 2 === 0 ? evenNums.push(nums[i]): null;
+  }
+   return evenNums;
+}
 
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
